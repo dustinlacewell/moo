ac7e3d53db63e5dea7e91e86d4129d648dba0053
Adam <Adam@anope.org>
2011-10-02 14:11:13 -0400

    Updated config & take invites from idle channels

--- src/net/rizon/moo/messages/messageInvite.java
+++ src/net/rizon/moo/messages/messageInvite.java
@@ -13,12 +13,22 @@ public class messageInvite extends message
 	@Override
 	public void run(String source, String[] message)
 	{
-		if (message.length > 1 && message[0].equalsIgnoreCase(moo.conf.getNick()) && moo.conf.getChannels() != null)
-			for (int i = 0; i < moo.conf.getChannels().length; ++i)
-				if (moo.conf.getChannels()[i].equalsIgnoreCase(message[1]))
-				{
-					moo.sock.join(message[1]);
-					break;
-				}
+		if (message.length > 1 && message[0].equalsIgnoreCase(moo.conf.getNick()))
+		{
+			if (moo.conf.getChannels() != null)
+				for (int i = 0; i < moo.conf.getChannels().length; ++i)
+					if (moo.conf.getChannels()[i].equalsIgnoreCase(message[1]))
+					{
+						moo.sock.join(message[1]);
+						break;
+					}
+			if (moo.conf.getIdleChannels() != null)
+				for (int i = 0; i < moo.conf.getIdleChannels().length; ++i)
+					if (moo.conf.getIdleChannels()[i].equalsIgnoreCase(message[1]))
+					{
+						moo.sock.join(message[1]);
+						break;
+					}
+		}
 	}
 }
