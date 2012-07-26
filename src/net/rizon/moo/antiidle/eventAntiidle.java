0207e45a402ae92039360c0a624d8267d9c7d956
Adam <Adam@anope.org>
2012-07-26 03:57:42 -0400

    Sometimes it helps to add new files

---
+++ src/net/rizon/moo/antiidle/eventAntiidle.java
@@ -0,0 +1,53 @@
+package net.rizon.moo.antiidle;
+
+import net.rizon.moo.event;
+import net.rizon.moo.moo;
+
+class eventAntiIdle extends event
+{
+	@Override
+	public void onJoin(final String source, final String channel) 
+	{
+		if (moo.conf.getAntiIdleChannel().equalsIgnoreCase(channel) == false || moo.conf.getNick().equals(source))
+			return;
+		
+		new antiIdleEntry(source).start();
+	}
+	
+	@Override
+	public void onPart(final String source, final String channel)
+	{
+		if (moo.conf.getAntiIdleChannel().equalsIgnoreCase(channel) == false || moo.conf.getNick().equals(source))
+			return;
+		
+		antiIdleEntry.removeTimerFor(source);
+	}
+
+	@Override
+	public void onKick(final String source, final String target, final String channel)
+	{
+		if (moo.conf.getAntiIdleChannel().equalsIgnoreCase(channel) == false || moo.conf.getNick().equals(target))
+			return;
+		
+		antiIdleEntry.removeTimerFor(target);
+	}
+	
+	@Override
+	public void onMode(final String source, final String channel, final String modes)
+	{
+		if (moo.conf.getAntiIdleChannel().equalsIgnoreCase(channel) == false)
+			return;
+		
+		for (final String s : modes.split(" "))
+			antiIdleEntry.removeTimerFor(s);
+	}
+	
+	@Override
+	public void onPrivmsg(final String source, final String channel, final String[] message)
+	{
+		if (moo.conf.getAntiIdleChannel().equalsIgnoreCase(channel) == false)
+			return;
+		
+		antiIdleEntry.removeTimerFor(source);
+	}
+}
