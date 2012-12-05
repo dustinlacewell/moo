b8079f7ac31217be89b686014fd51d56ec3904ec
CuleX <cculex@gmail.com>
2012-12-05 15:26:21 +0100

    commits: Respond to bitbucket so it doesn't send twice.

--- src/net/rizon/moo/commits/server.java
+++ src/net/rizon/moo/commits/server.java
@@ -1,8 +1,10 @@
 package net.rizon.moo.commits;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -35,6 +37,7 @@ class server extends Thread
 			for (Socket client; (client = sock.accept()) != null;)
 			{
 				BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				BufferedWriter output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 				
 				for (String str; (str = input.readLine()) != null;)
 					if (str.isEmpty())
@@ -62,6 +65,11 @@ class server extends Thread
 					}
 				}
 				
+				output.write("HTTP/1.1 200 OK\r\n");
+				output.write("Content-Type: text/html; charset=UTF-8\r\n");
+				output.write("Connection: close\r\n");
+				output.write("Content-Length: 2\r\n\r\nOK");
+				output.close();
 				client.close();
 			}
 			
