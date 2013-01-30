c099d331e58d35b48081fb001c81d00d47f1f0a1
Adam <Adam@anope.org>
2013-01-30 11:43:09 -0500

    Use a less dumb build.xml (somewhat stolen from our bncbot)

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. ./build.sh
-
-tar zcf moo.tar.gz moo.jar lib *.template run.sh servercontrol
-rm -f moo.jar
-
--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-java -XX:+HeapDumpOnOutOfMemoryError -Xmx16M -jar moo.jar >moo.log 2>&1 &
+if [ `grep ^debug=0$ moo.properties` ] ; then
+	java -XX:+HeapDumpOnOutOfMemoryError -Xmx16M -jar moo.jar >moo.log 2>&1 &
+	echo "moo started"
+else
+	java -XX:+HeapDumpOnOutOfMemoryError -Xmx16M -jar moo.jar
+fi
+
