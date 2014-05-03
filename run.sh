2ea3c876d613f59492fc4824971f5345c816b1f9
Adam <Adam@anope.org>
2014-05-02 21:29:56 -0400

    Build a jar with dependencies, otherwise it cannot find sqlite

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+JAR=target/moo-2.0-SNAPSHOT-jar-with-dependencies.jar
+
 if [ `grep ^debug=0$ moo.properties` ] ; then
-	java -XX:+HeapDumpOnOutOfMemoryError -Xmx16M -jar moo.jar >moo.log 2>&1 &
+	java -XX:+HeapDumpOnOutOfMemoryError -Xmx16M -jar $JAR >moo.log 2>&1 &
 	echo "moo started"
 else
-	java -XX:+HeapDumpOnOutOfMemoryError -Xmx16M -jar moo.jar
+	java -XX:+HeapDumpOnOutOfMemoryError -Xmx16M -jar $JAR
 fi
 
