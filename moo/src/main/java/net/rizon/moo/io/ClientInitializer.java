e396ccc07e7db79a6602d7a66311a6165f4433b7
Orillion <orillion@rizon.net>
2015-09-04 01:35:31 +0200

    Allow moo to connect using Certificate + PKCS8 key

--- moo/src/main/java/net/rizon/moo/io/ClientInitializer.java
+++ moo/src/main/java/net/rizon/moo/io/ClientInitializer.java
@@ -8,9 +8,23 @@ import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.ssl.IdentityCipherSuiteFilter;
 import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.ssl.SslProvider;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.timeout.IdleStateHandler;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.nio.file.Files;
+import java.security.KeyFactory;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
 import net.rizon.moo.Moo;
 
 public class ClientInitializer extends ChannelInitializer<SocketChannel>
@@ -33,12 +47,30 @@ public class ClientInitializer extends ChannelInitializer<SocketChannel>
 		{
 			SslContext sslCtx;
 			File certificate = null;
-			if (Moo.conf.general.cert != null)
+			if (Moo.conf.general.cert != null && Moo.conf.general.key != null)
 			{
-				certificate = new File(Moo.conf.general.cert);
+				CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+				byte[] certBytes = Files.readAllBytes(new File(Moo.conf.general.cert).toPath());
+				byte[] keyBytes = Files.readAllBytes(new File(Moo.conf.general.key).toPath());
+				X509Certificate cert = (X509Certificate) certFactory.generateCertificate(new ByteArrayInputStream(certBytes));
+				PrivateKey key = (PrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(keyBytes));
+				KeyStore ks = KeyStore.getInstance("JKS");
+				ks.load(null);
+				ks.setCertificateEntry("moo", cert);
+				ks.setKeyEntry("moo", key, "".toCharArray(), new Certificate[] { cert });
+				KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+				kmf.init(ks, "".toCharArray());
+				SSLContext clientContext = SSLContext.getInstance("TLS");
+				clientContext.init(kmf.getKeyManagers(), InsecureTrustManagerFactory.INSTANCE.getTrustManagers(), null);
+				SSLEngine engine = clientContext.createSSLEngine();
+				engine.setUseClientMode(true);
+				pipeline.addLast("ssl", new SslHandler(engine));
 			}
-			sslCtx = SslContext.newClientContext(certificate, InsecureTrustManagerFactory.INSTANCE, null, IdentityCipherSuiteFilter.INSTANCE, null, 16, 10);
-			pipeline.addLast("ssl", sslCtx.newHandler(ch.alloc()));
+			else
+			{
+				sslCtx = SslContext.newClientContext(null, null, InsecureTrustManagerFactory.INSTANCE, null, IdentityCipherSuiteFilter.INSTANCE, null, 16, 10);
+				pipeline.addLast("ssl", sslCtx.newHandler(ch.alloc()));
+			}
 		}
 
 		pipeline.addLast("frameDecoder", new LineBasedFrameDecoder(MAXBUF));
