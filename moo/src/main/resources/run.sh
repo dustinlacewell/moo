524aa79af79d27675b2bb8afc65f9090cc110553
Adam <Adam@anope.org>
2016-02-26 11:41:15 -0500

    nix fixes

---
+++ moo/src/main/resources/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java $* > /dev/null 2>&1 &
--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-JAR=moo/target/moo-moo-3.0-jar-with-dependencies.jar
-ARGS='-XX:+HeapDumpOnOutOfMemoryError -Xmx16M'
+mvn exec:exec -pl moo
 
-if grep -q -E '^debug: false$' moo.yml ; then
-	java $ARGS -jar $JAR >moo.log 2>&1 &
-	echo "moo started"
-else
-	java $ARGS -jar $JAR
-fi
+#if grep -q -E '^debug: false$' moo.yml ; then
+#	java $ARGS -jar $JAR >moo.log 2>&1 &
+#	echo "moo started"
+#else
+#	java $ARGS -jar $JAR
+#fi
 
