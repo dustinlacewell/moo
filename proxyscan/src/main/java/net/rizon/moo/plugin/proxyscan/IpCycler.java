b0c9cdabf6c1f5762a68350e8021e50fcf9221e7
Adam <Adam@anope.org>
2016-08-22 17:35:27 -0400

    proxyscan: allow none/empty bindips

--- proxyscan/src/main/java/net/rizon/moo/plugin/proxyscan/IpCycler.java
+++ proxyscan/src/main/java/net/rizon/moo/plugin/proxyscan/IpCycler.java
@@ -8,15 +8,15 @@ public class IpCycler
 	public IpCycler(String[] ips)
 	{
 		this.ips = ips;
-
-		if (ips.length == 0)
-		{
-			throw new IllegalArgumentException();
-		}
 	}
 
 	public String getIp()
 	{
+		if (ips == null || ips.length == 0)
+		{
+			return null;
+		}
+
 		if (curIp >= ips.length)
 		{
 			curIp = 0;
