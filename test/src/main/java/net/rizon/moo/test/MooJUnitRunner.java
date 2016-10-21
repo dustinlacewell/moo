ae14f1c4904f70f04ff3a8f69fb0b16ab449f02a
Adam <Adam@anope.org>
2016-10-21 14:11:13 -0400

    junit runner: verify like mockito does after test is finished

--- test/src/main/java/net/rizon/moo/test/MooJUnitRunner.java
+++ test/src/main/java/net/rizon/moo/test/MooJUnitRunner.java
@@ -2,8 +2,13 @@ package net.rizon.moo.test;
 
 import com.google.inject.Guice;
 import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 /**
@@ -25,12 +30,35 @@ public class MooJUnitRunner extends BlockJUnit4ClassRunner
 		Object testObject = super.createTest();
 
 		// Perform stuff on member variables here
-
 		// Init mocks first, else we're binding null objects
 		MockitoAnnotations.initMocks(testObject);
 		// Inject everything in the test object
 		Guice.createInjector(BoundFieldModule.of(testObject)).injectMembers(testObject);
 
 		return testObject;
+	}
+
+	@Override
+	public void run(final RunNotifier notifier)
+	{
+		notifier.addListener(new RunListener()
+		{
+			@Override
+			public void testFinished(Description description) throws Exception
+			{
+				super.testFinished(description);
+				
+				try
+				{
+					Mockito.validateMockitoUsage();
+				}
+				catch (Throwable t)
+				{
+					notifier.fireTestFailure(new Failure(description, t));
+				}
+			}
+		});
+
+		super.run(notifier);
 	}
 }
